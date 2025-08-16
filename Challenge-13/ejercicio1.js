ea1d27854406e743288fa09f2ac2686a7e40a2c8
Nahuel Nuñez <nunezn53@gmail.com>
2025-08-16 00:37:07 -0300

    completo ejercicio 2 challenge 13

--- Challenge-13/ejercicio1.js
+++ Challenge-13/ejercicio1.js
@@ -1,5 +1,5 @@
 //crea un bucle que imprima los numeros del 1 al 20
 
-for (let i = 1; i <= 20 ; i++) {
-    document.writeln(i + "<br>");
+for (let num = 1; num <= 20 ; num++) {
+    document.writeln(num + "<br>");
 }
