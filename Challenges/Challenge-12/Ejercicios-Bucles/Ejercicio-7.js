027cf36b912229ae25acc4d95381999e9a852fea
Nahuel Nuñez <nunezn53@gmail.com>
2025-09-15 06:00:09 -0300

    agrego ejercicios bucles faltantes

--- Challenges/Challenge-12/Ejercicios-Bucles/Ejercicio-7.js
+++ Challenges/Challenge-12/Ejercicios-Bucles/Ejercicio-7.js
@@ -1,2 +1,16 @@
 //Solicitar números y sumarlos hasta que el total sea igual o mayor que 100.
 
+
+let total = 0;
+
+let numero1 = parseInt(prompt("Ingresa el primer número:"));
+let numero2 = parseInt(prompt("Ingresa el segundo número:"));
+
+while (total < 100) {
+  
+    total += numero1 + numero2;
+    
+}
+
+alert(`¡Felicidades! El total final es: ${total}`);
+
