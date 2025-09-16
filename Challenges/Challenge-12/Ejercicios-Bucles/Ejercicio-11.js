6701a0fd4625650dd5947a406f78e34ce4033534
Nahuel Nuñez <nunezn53@gmail.com>
2025-09-16 05:32:55 -0300

    agrego ejercicios bucles

---
+++ Challenges/Challenge-12/Ejercicios-Bucles/Ejercicio-11.js
@@ -0,0 +1,14 @@
+//Mostrar opciones al usuario y repetir hasta que elija salir.
+
+let opcion;
+
+do {
+    opcion = parseInt(prompt("1. Primera Opcion \n2. Segunda Opcion \n3. Tercera Opcion \n4. Salir"))
+
+     /* if (opcion === 1) {
+        
+    } else {
+        
+    }  */
+
+} while (opcion !== 4);
---
+++ Challenges/Challenge-12/Ejercicios-Bucles/Ejercicio-9.js
@@ -0,0 +1,13 @@
+//Solicitar la edad al usuario y repetir mientras no esté en el rango (18–99).
+
+//let edad = parseInt(prompt("Ingrese su edad"))
+
+let edad = 2
+
+while (edad <= 18 || edad >= 99) {
+
+    edad = edad + edad 
+
+}
+
+console.log(edad);
