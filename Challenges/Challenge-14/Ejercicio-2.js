1ee97432aaa06ef94c933fa85535c049b6538c12
Nahuel Nuñez <nunezn53@gmail.com>
2025-09-20 23:33:00 -0300

    Agrego ejercicio 2 de challenge 14

---
+++ Challenges/Challenge-14/Ejercicio-2.js
@@ -0,0 +1,19 @@
+//Crea una función que reciba un array y un elemento, y lo agregue al final usando .push().
+
+let array = ["1", "2", "3", "4"]
+
+let elemento2 = "5" //prompt("Ingrese una carta de clash royale")
+
+
+function AgregarElemento(a,b) {
+    b.push(a)
+    return b //devuelve el arreglo ya modificado.
+}
+
+console.log(AgregarElemento(elemento2,array));
+
+
+/////////
+
+
+
