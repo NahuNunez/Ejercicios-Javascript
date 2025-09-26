732b2c2e5c9bedec3db20baecc311890e46f5f62
Nahuel Nuñez <nunezn53@gmail.com>
2025-09-26 05:36:04 -0300

    Termino ejercicio 1 del challenge 15

---
+++ Challenges/Challenge-15/Ejercicio-1.js
@@ -0,0 +1,47 @@
+
+const productos =  [ 
+{ id: 1, nombre: "Laptop", precio: 1200, categoria: "tecnologia", stock: 15 },
+{ id: 2, nombre: "Mouse", precio: 25, categoria: "tecnologia", stock: 0 },
+{ id: 3, nombre: "Silla", precio: 150, categoria: "muebles", stock: 8 },
+{ id: 4, nombre: "Monitor", precio: 300, categoria: "tecnologia", stock: 12 },
+{ id: 5, nombre: "Teclado", precio: 45, categoria: "tecnologia", stock: 0 } ]
+
+
+//1 Mostrar todos los productos que tienen stock disponible
+
+const stockDisponible = productos.filter(function (producto) {
+    return producto.stock > 0
+})
+
+console.log(stockDisponible)
+
+
+//2 Crear un array solo con los nombres y precios de los productos
+
+const arrayNuevo = productos.map(function (producto) {
+    return {nombre: producto.nombre , precio: producto.precio}
+})
+
+console.log(arrayNuevo);
+
+
+//Filtrar los productos de tecnología que tienen stock
+
+const stocktech = productos.filter(function (producto) {
+    if (producto.categoria === "tecnologia" && producto.stock > 0) {
+        return producto
+    }
+})
+
+console.log(stocktech);
+
+
+//Encontrar el producto llamado "Monitor"
+
+const EncontrarMonitor = productos.find( function (producto) {
+    if (producto.nombre === "Monitor"){
+        return producto
+    }
+}) 
+
+console.log(EncontrarMonitor);
