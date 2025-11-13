3b4c761c3fdb2a8518c981cd865f4ee82baf3848
Nahuel Nuñez <nunezn53@gmail.com>
2025-11-13 06:10:13 -0300

    agrego lista tareas

--- Challenges/Challenge-17/app.js
+++ Challenges/Challenge-17/app.js
@@ -38,10 +38,10 @@ const actualizarReloj = () => {
   const horas = actual.toLocaleTimeString();
   //const minutos = actual.getMinutes();
   //const segundos = actual.getSeconds();
+
   /* - se extrae la hora con getHours (devuelve un número entre 0 y 23); 
      - se convierte en un string para que se pueda aplicar .padStart
      - .padStart: Asegura que el número tenga dos dígitos (2 , '0') el primer parametro es el largo total de la cadena y el segundo es el relleno para que no rompa el formato de dos dígitos 
-
    */
 
   const reloj = document.getElementById("reloj");
@@ -49,3 +49,43 @@ const actualizarReloj = () => {
 };
 
 setInterval(actualizarReloj, 1000); // cada segundo, se ejecuta actualizarReloj() y actualiza la hora en pantalla
+
+
+
+//////////// Lista de Tareas ///////////
+
+/* obtengo por id el button */
+const botonTarea = document.getElementById("button-addon2") 
+
+/* obtengo por id el div para las tareas */
+const listaTareas = document.getElementById("listaTareas");
+
+
+/* creo la funcion creaTarea que hara toda la logica */
+const crearTarea = () => {
+
+    /* guardo en la variable inputTarea lo que llamo por id al input de la tarea */
+    const inputTarea = document.getElementById("inputTarea")
+
+    /* guardo en la variable texto lo que se escribio en el input */
+    const texto = inputTarea.value 
+
+    //creo el contendedor de la tarea
+      const listGroupItem = document.createElement("div");
+      listGroupItem.className = "list-group-item d-flex justify-content-between align-items-center";
+
+      
+    //creo el contenido de la tarea
+      const tareaContent = document.createElement("span");
+      tareaContent.className = "flex-grow-1";
+      /* con textContent se guarda lo que se escribio en el input */
+      tareaContent.textContent = texto;
+
+
+      listGroupItem.appendChild(tareaContent);
+      listaTareas.appendChild(listGroupItem);
+
+      inputTarea.value = "";
+}
+
+ botonTarea.addEventListener("click", crearTarea);
